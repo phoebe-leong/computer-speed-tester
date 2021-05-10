7dbbe58012710e3a3d073ae06be5b7e5e028010d
PH03be <phoebeleong8@gmail.com>
2021-05-10 12:30:47 +1000

    shows length of time to run program after finish

--- main.cpp
+++ main.cpp
@@ -1,2 +1,4 @@
 #include <iostream>
-int main() { system("clear"); int i = 0; bool run = true; while(run == true) { i++; if (i >= 999999999) { run = false; } if (run == false) { system("clear"); std::cout << "Finished!"; std::cin.get(); return 0; } } }
+#include <chrono>
+bool run = true;
+void program () { system("clear"); int i = 0; while(run == true) { i++; if (i >= 999999999) { run = false; } } } int main() { using std::chrono::high_resolution_clock; using std::chrono::duration_cast; using std::chrono::duration; using std::chrono::milliseconds; std::chrono::steady_clock::time_point t1 = high_resolution_clock::now(); program(); std::chrono::steady_clock::time_point t2 = high_resolution_clock::now(); duration<double> secs = t2 - t1; if (run == false) { system("clear"); std::cout << "Finished in " << secs.count() << " seconds!"; std::cin.get(); return 0; } } 
